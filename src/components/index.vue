0ab15d96df3c6863341451b5dbfd60045dc2e035
zifeifish <51848087+zifeifish@users.noreply.github.com>
2019-07-26 17:43:35 +0800

    使用 elementUI布局容器-初始化后台页面

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,13 +1,31 @@
 <template>
-    <h1>后台管理</h1>
+  <el-container class="mian_container">
+    <el-header class="header">Header</el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-main class="my_main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.mian_container {
+  height: 100%;
+  .header {
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    background-color: #b3c0d1;
+  }
+  .aside {
+    background-color: #fff;
+  }
+  .my_main {
+    background-color: #e9eef3;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // 导入 vue-router 模块
 import VueRouter from 'vue-router';
