b5fb756763ee28a13e97075a6431957eb34e785c
zifeifish <51848087+zifeifish@users.noreply.github.com>
2019-07-27 15:02:14 +0800

    后台页面顶部栏布局完成(弹性布局)

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,10 @@
 <template>
   <el-container class="mian_container">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <img src="../assets/logo.png" alt="">
+      <h2>gobuy电商后台管理系统</h2>
+      <a href="#" class="singout">退出</a>
+    </el-header>
     <el-container>
       <el-aside width="200px" class="aside">Aside</el-aside>
       <el-main class="my_main">Main</el-main>
@@ -20,6 +24,23 @@ export default {};
     height: 60px;
     line-height: 60px;
     background-color: #b3c0d1;
+    // 伸缩布局
+    display: flex;
+    // 主轴对其方式
+    justify-content: space-between;
+    // 次轴对其方式
+    align-items: center;
+    img{
+      width: 180px;
+      height: 60px;
+    }
+    h2{
+      display: inline-block;
+      color:cornflowerblue;
+    }
+    .singout{
+      color: brown;
+    }
   }
   .aside {
     background-color: #fff;
