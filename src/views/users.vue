68c6df6dee2a1da16bd512d57ecd8e5c202ec6f1
zifeifish <51848087+zifeifish@users.noreply.github.com>
2019-07-30 16:23:06 +0800

    完成用户数据列表分页功能

--- src/views/users.vue
+++ src/views/users.vue
@@ -11,75 +11,125 @@
         </el-input>
       </el-col>
       <el-col :span="12">
-        <el-button type="success" plain>添加用户</el-button>
+        <el-button type="success" plain @click="dialogFormVisible = true">添加用户</el-button>
       </el-col>
     </el-col>
     <!-- 表格栏 -->
     <el-table :data="tableData" border style="width: 100%">
-      <el-table-column prop="id" label="#" width="40"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="160"></el-table-column>
+      <el-table-column type="index"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="160"></el-table-column>
       <el-table-column prop="email" label="邮箱" width="300"></el-table-column>
-      <el-table-column prop="tel" label="电话" width="280"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="280"></el-table-column>
       <el-table-column prop="status" label="用户状态" width="80">
-        <!-- 开关 -->
-        <el-switch
-          v-model="value"
-          active-color="#13ce66"
-          inactive-color="#ff4949"
-          @change="handleChange"
-        ></el-switch>
+        <template slot-scope="scope">
+          <!-- 开关 -->
+          <!-- scope.row 保存的是当前自定义列里的数据 -->
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
       </el-table-column>
       <el-table-column prop="option" label="操作">
-        <el-button type="primary" icon="el-icon-edit" plain size="mini"></el-button>
-        <el-button type="danger" icon="el-icon-delete" plain size="mini"></el-button>
-        <el-button type="warning" icon="el-icon-check" plain size="mini"></el-button>
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" plain size="mini"></el-button>
+          <el-button type="danger" icon="el-icon-delete" plain size="mini"></el-button>
+          <el-button type="warning" icon="el-icon-check" plain size="mini"></el-button>
+        </template>
       </el-table-column>
     </el-table>
     <!-- 分页 -->
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
-      :current-page="1"
-      :page-sizes="[5, 10, 15, 20]"
-      :page-size="100"
+      :current-page="usersData.pagenum"
+      :page-sizes="[3, 5,10]"
+      :page-size="usersData.pagesize"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="10"
+      :total="total"
     ></el-pagination>
+
+    <!-- 添加用户弹出框 -->
+    <!-- <el-dialog title="添加用户" :visible.sync="dialogFormVisible">
+          <el-form status-icon label-width="100px" class="demo-ruleForm" label-position="left">
+            <el-form-item label="用户名" prop>
+              <el-input type="text" autocomplete="off" :label-width="formLabelWidth"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+              <el-input type="password" autocomplete="off" :label-width="formLabelWidth"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="password">
+              <el-input type="email" autocomplete="off" :label-width="formLabelWidth"></el-input>
+            </el-form-item>
+            <el-form-item label="电话" prop="password">
+              <el-input type="tel" autocomplete="off" :label-width="formLabelWidth"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+          </div>
+    </el-dialog>-->
   </div>
 </template>
 
 <script>
+// 导入http 发请求
+import { http } from "../api/http";
+
 export default {
   data() {
     return {
-      
-      tableData: [
-        {
-          id: 1,
-          name: "王小虎",
-          email: "adsfad@qq.com",
-          tel: "666",
-          status: "正常",
-          option: ""
-        },
-        {
-          id: 2,
-          name: "王小虎",
-          email: "",
-          tel: "",
-          status: "",
-          option: ""
-        }
-      ],
-      value: true,
+      // 用户列表查询参数
+      usersData: {
+        query: "",
+        pagenum: 1,
+        pagesize: 3
+      },
+      // 数据总条数
+      total: "",
+      // 表格数据
+      tableData: [],
+      // 对话框参数
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      },
+      formLabelWidth: "120px"
     };
   },
   methods: {
-    handleSizeChange() {},
-    handleCurrentChange() {},
-    handleChange(val) {
-      console.log(val);
+    // 封装获取用户数据列表 方法
+    getUsersData() {
+      http.users(this.usersData).then(backData => {
+        console.log(backData);
+        // 表格数据
+        this.tableData = backData.data.data.users;
+        // 总数据量
+        this.total = backData.data.data.total;
+      });
+    },
+    // 选择每页显示数量
+    handleSizeChange(pagesize) {
+      // 修改要显示的条数
+      this.usersData.pagesize = pagesize;
+      this.getUsersData();
+    },
+    // 点击页码
+    handleCurrentChange(pagenum) {
+      // 修改当前页码
+      this.usersData.pagenum = pagenum;
+      this.getUsersData();
     }
+  },
+  created() {
+    // 页面一进来 发送axjx 获取用户数据列表
+    this.getUsersData();
   }
 };
 </script>
