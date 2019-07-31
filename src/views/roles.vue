6d9d6fcdb4efb786b0faadf0174e35b313dcae7b
zifeifish <51848087+zifeifish@users.noreply.github.com>
2019-07-31 16:14:10 +0800

    完成编辑角色功能

--- src/views/roles.vue
+++ src/views/roles.vue
@@ -3,7 +3,7 @@
     <!-- 面包屑导航栏 -->
     <bread first="权限管理" second="角色列表"></bread>
     <el-button @click="addFormVisible=true">添加角色</el-button>
-    <!-- 表格栏 -->
+    <!-- 展开行表格栏 -->
     <template>
       <el-table border :data="tableData" style="width: 100%">
         <el-table-column type="expand">
@@ -22,9 +22,20 @@
         <el-table-column prop="roleName" label="角色名称" width="380"></el-table-column>
         <el-table-column prop="roleDesc" label="角色描述" width="380"></el-table-column>
         <el-table-column prop="address" label="操作">
-          <el-button type="primary" icon="el-icon-edit" plain size="mini"></el-button>
-          <el-button type="danger" icon="el-icon-delete" plain size="mini"></el-button>
-          <el-button type="warning" icon="el-icon-check" plain size="mini"></el-button>
+          <template slot-scope="scope">
+            <!-- 编辑 -->
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              plain
+              size="mini"
+              @click="openEdit(scope.row.id)"
+            ></el-button>
+            <!-- 删除 -->
+            <el-button type="danger" icon="el-icon-delete" plain size="mini"></el-button>
+            <!-- 修改 -->
+            <el-button type="warning" icon="el-icon-check" plain size="mini"></el-button>
+          </template>
         </el-table-column>
       </el-table>
     </template>
@@ -48,6 +59,27 @@
       <div slot="footer" class="dialog-footer">
         <el-button @click="addFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="submitForm('form')">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 编辑角色对话框 -->
+    <el-dialog title="添加角色" :visible.sync="editFormVisible">
+      <el-form
+        :model="editForm"
+        :rules="editrules"
+        label-width="100px"
+        class="demo-ruleForm"
+        label-position="right"
+      >
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input type="text" autocomplete="off" v-model="editForm.roleName"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" prop="roleDesc">
+          <el-input type="text" autocomplete="off" v-model="editForm.roleDesc"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editSubmitForm(editForm.roleId)">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -74,7 +106,22 @@ export default {
           { required: true, message: "请输入角色描述", trigger: "blur" }
         ]
       },
-      addFormVisible: false // 添加角色对话框 默认隐藏
+      addFormVisible: false, // 添加角色对话框 默认隐藏
+      // 编辑角色 表单数据
+      editForm: {
+        roleName: "",
+        roleDesc: ""
+      },
+      // 编辑角色 表单验证
+      editrules: {
+        roleName: [
+          { required: true, message: "请输入角色名称", trigger: "blur" }
+        ],
+        roleDesc: [
+          { required: true, message: "请输入角色描述", trigger: "blur" }
+        ]
+      },
+      editFormVisible: false // 编辑角色对话框 默认隐藏
     };
   },
   methods: {
@@ -86,7 +133,7 @@ export default {
       });
     },
 
-    // 表单提交
+    // 添加角色 表单提交
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
@@ -109,6 +156,36 @@ export default {
         } else {
           console.log("error submit!!");
           return false;
+        }
+      });
+    },
+
+    // 点击编辑按钮
+    openEdit(id) {
+      // 显示编辑角色对话框
+      this.editFormVisible = true;
+      // 获取角色信息
+      http.get(`roles/${id}`).then(backData => {
+        console.log(backData);
+        // 获取编辑角色信息
+        this.editForm = backData.data.data;
+      });
+    },
+
+    // 编辑角色 表单提交
+    editSubmitForm(id) {
+      http.put(`roles/${id}`, this.editForm).then(backData => {
+        console.log(backData);
+        if (backData.data.meta.status == 200) {
+          // 提示更新成功
+          this.$message.success('编辑成功');
+          // 关闭对话框
+          this.editFormVisible = false;
+          // 更新页面数据
+          this.getRolesList();
+        } else {
+          // 提示 未添加成功信息
+          this.$message.error(backData.data.meta.msg);
         }
       });
     }
